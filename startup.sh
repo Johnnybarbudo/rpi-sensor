4de415ed60ba77b89500b646216b7710183fa863
YannickKeyssner <y.keyssner@tu-bs.de>
2022-03-08 14:55:54 +0000

    changed IP address

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 eval `ssh-agent`
 ssh-add
-autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -R 10001:localhost:22 pi@130.211.204.106
+autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -R 10001:localhost:22 pi@192.168.1.133
 export GOOGLE_APPLICATION_CREDENTIALS="serviceaccount_pubsub.json"
