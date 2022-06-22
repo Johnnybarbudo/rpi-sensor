6f825c386b3a1261208ffa770009d0ae67048cba
Simon Szalai <simon.szalai@gmail.com>
2022-06-22 12:32:33 -0700

    change ssh host

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 eval `ssh-agent`
 ssh-add
-autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -R 10001:localhost:22 pi@192.168.1.133
+autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -fN -R 10001:localhost:22 simon@104.199.69.122
 export GOOGLE_APPLICATION_CREDENTIALS="serviceaccount_pubsub.json"
